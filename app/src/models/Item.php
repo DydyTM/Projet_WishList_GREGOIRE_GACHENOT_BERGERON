ab9ae96c6284cc163931dee0f03194146ee12e6e
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-12-03 09:10:18 +0100

    Ajout du pretty printing

--- app/src/models/Item.php
+++ app/src/models/Item.php
@@ -1,20 +1,28 @@
 <?php
 
 
-namespace wishlist\models;
+namespace wishlist\models {
 
-class Item extends \Illuminate\Database\Eloquent\Model
-{
+    class Item extends \Illuminate\Database\Eloquent\Model
+    {
 
-    protected $table = 'item';
-    protected $primarykey = 'id';
-    protected $liste_id = 'liste_id';
-    protected $nom = 'nom';
-    protected $descr = 'descr';
-    protected $img = 'img';
-    protected $url = 'url';
-    protected $tarif = 'tarif';
-    public $timestamps = false;
+        protected $table = 'item';
+        protected $primarykey = 'id';
+        protected $liste_id = 'liste_id';
+        protected $nom = 'nom';
+        protected $descr = 'descr';
+        protected $img = 'img';
+        protected $url = 'url';
+        protected $tarif = 'tarif';
+        public $timestamps = false;
 
 
+    }
+
+}
+
+namespace wishlist\models\pretty\item {
+    function pprint($i) {
+        return "Item #" . $i['liste_id'] . ":" . $i['id'] . ": " . $i['nom'] . " ; " . $i['descr'] . " ; " . $i['tarif'];
+    }
 }
--- app/src/models/Liste.php
+++ app/src/models/Liste.php
@@ -1,17 +1,23 @@
 <?php
 
-namespace wishlist\models;
+namespace wishlist\models {
+    class Liste extends \Illuminate\Database\Eloquent\Model {
 
-class Liste extends \Illuminate\Database\Eloquent\Model {
-
-    protected $table = 'liste';
-    protected $primarykey = 'id';
-    protected $user_id = 'user_id';
-    protected $titre = 'titre';
-    protected $description = 'description';
-    protected $expiration = 'expiration';
-    protected $token = 'token';
-    public $timestamps = false;
+        protected $table = 'liste';
+        protected $primarykey = 'id';
+        protected $user_id = 'user_id';
+        protected $titre = 'titre';
+        protected $description = 'description';
+        protected $expiration = 'expiration';
+        protected $token = 'token';
+        public $timestamps = false;
 
 
+    }
+}
+
+namespace wishlist\models\pretty\liste {
+    function pprint($l) {
+        return "Liste #" . $l['no'] . " of user #" . $l['user_id'] . ": " . $l['titre'] . " ; " . $l['description'] . " ; Expires: " . $l['expiration'];
+    }
 }
