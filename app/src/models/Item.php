de3d059a8e33acedfa3e0f42e2fccfe95ed24866
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-12-03 09:38:29 +0100

    Merge branch 'master' of https://github.com/Dydouche/Projet_WishList_GREGOIRE_GACHENOT_BERGERON

--- app/src/models/Item.php
+++ app/src/models/Item.php
@@ -6,15 +6,15 @@ namespace wishlist\models {
     class Item extends \Illuminate\Database\Eloquent\Model
     {
 
-        protected $table = 'item';
-        protected $primarykey = 'id';
-        protected $liste_id = 'liste_id';
-        protected $nom = 'nom';
-        protected $descr = 'descr';
-        protected $img = 'img';
-        protected $url = 'url';
-        protected $tarif = 'tarif';
-        public $timestamps = false;
+    protected $table = 'item';
+    protected $id = 'id';
+    protected $liste_id = 'liste_id';
+    protected $nom = 'nom';
+    protected $descr = 'descr';
+    protected $img = 'img';
+    protected $url = 'url';
+    protected $tarif = 'tarif';
+    public $timestamps = false;
 
 
     }
@@ -23,6 +23,6 @@ namespace wishlist\models {
 
 namespace wishlist\models\pretty\item {
     function pprint($i) {
-        return "Item #" . $i['liste_id'] . ":" . $i['id'] . ": " . $i['nom'] . " ; " . $i['descr'] . " ; " . $i['tarif'];
+        return "Item #" . $i['liste_id'] . ":" . $i['id'] . ": " . $i['nom'] . " ; " . $i['descr'] . ", " . $i['tarif'] . " €";
     }
 }
