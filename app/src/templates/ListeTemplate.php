937ba4bcbab7a42f48e440420e1621e20689a05c
Raikuji <irub.tech@gmail.com>
2019-12-10 15:21:15 +0100

    Modif du css

--- app/src/templates/ListeTemplate.php
+++ app/src/templates/ListeTemplate.php
@@ -9,26 +9,28 @@ class ListeTemplate extends Template {
         return "
             <form action=liste method=POST>
                 <link href=\"" . T::$CSS_BASE_DIR . "/my-wishlist.css\" rel=\"stylesheet\">
-                <div class=\"nouvelleListe\">Nouvelle liste à créer !</div>
-                <div class=\"form\" class \"desc\">
-                Titre :
+                <div class=\"nouvelleListe form\">Nouvelle liste à créer !</div>
+                <div class=\"form desc\">
+                    Titre :
                 </div>
-                <div class=\"form\" class \"input\">
-                <input type=text name=titre placeholder=\"Ma wishlist\"/>
+                <div class=\"form input\">
+                    <input type=text name=titre placeholder=\"Ma wishlist\"/>
                 </div>
-                <div class=\"form\" class \"desc\">
-                Description :
+                <div class=\"form desc\">
+                    Description :
                 </div>
-                <div class=\"form\" class \"input\">
-                <textarea type=text name=description placeholder=\"Une super wishlist\"></textarea>
+                <div class=\"form input\">
+                    <textarea type=text name=description placeholder=\"Une super wishlist\"></textarea>
                 </div>
-                <div class=\"form\" class \"desc\">
-                Date d'expiration :
+                <div class=\"form desc\">
+                    Date d'expiration :
                 </div>
-                <div class=\"form\" class \"input\">
-                <input type=date name=expiration />
+                <div class=\"form input\">
+                    <input type=date name=expiration />
                 </div>
-                <input type=submit value=OK>
+                <div class=\"form\">
+                    <input type=submit value=OK>
+                </div>
             </form>
         ";
     }
