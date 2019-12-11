3851cc15d13487ae6b796f2de35eea5d08cd5c4e
Raikuji <irub.tech@gmail.com>
2019-12-11 11:27:39 +0100

    ajout des liens sur les items

--- app/src/models/Item.php
+++ app/src/models/Item.php
@@ -29,10 +29,12 @@ namespace wishlist\models\pretty\item {
         $img = $i['img'];
         $format = "
             <div class=\"row\">
+            <a class=\"invisible-link\" href=\"/liste/" . $i['liste_id'] . "/item/" . $i['id'] . "\">
                 <img class=\"imageObjet\" src=\"/img/$img\" height='70' width='70'> 
                 <div>
                     <div><u>Item #" . $i['id'] . ":  " . $i['nom'] ."</u><br>" . $i['descr'] . "<br> Tarif : ". $i['tarif']. " €" . "</div>
                 </div>
+                </a>
             </div>";
 
         return $format;
