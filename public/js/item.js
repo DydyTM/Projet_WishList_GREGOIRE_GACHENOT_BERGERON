36cd29ed99285b006842a866b975b30bdddd0848
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-01-06 10:24:02 +0100

    Rectification ajout participant

--- public/js/item.js
+++ public/js/item.js
@@ -5,7 +5,7 @@ if(ajoutParticipant != undefined) {
     ajoutParticipant.addEventListener('submit', e => {
         e.preventDefault()
         const participant = e.target[0].value
-        const data = new FormData();
+        const data = new FormData()
         data.append('participant', escapeHTMLEncode(participant))
         const url = e.target.attributes[2].value
         fetch(url, {body: data, method: 'POST'})
@@ -14,7 +14,7 @@ if(ajoutParticipant != undefined) {
                     throw new Error('Impossible to add participant to item: ' + r.status)
             }).then(_ => {
                 alert('Participant bien ajouté')
-                window.location.href = url;
+                window.location.href = url
             }).catch(e => alert(e))
     })
 }
